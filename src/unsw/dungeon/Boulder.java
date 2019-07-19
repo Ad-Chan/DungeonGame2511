eea2ed431030a979df576f7c38d5a87dbc82ad90
Adrian <adrianccchan@gmail.com>
2019-07-19 21:41:44 +1000

    Added work in progress boulder movement. Needs to check for other boulders + walls

--- src/unsw/dungeon/Boulder.java
+++ src/unsw/dungeon/Boulder.java
@@ -8,6 +8,28 @@ public class Boulder extends Entity {
 	}
 	
 	//Change position of boulder
+	public void checkPlayerPos(int playerX, int playerY) {
+		//Left
+		if (playerX > this.getX() && playerY == this.getY()) {
+			moveBoulder(this.getX()-1, this.getY());
+		}
+		
+		//Right
+		if (playerX < this.getX() && playerY == this.getY()) {
+			moveBoulder(this.getX()+1, this.getY());			
+		}		
+		
+		//Up
+		if (playerX == this.getX() && playerY > this.getY()) {
+			moveBoulder(this.getX(), this.getY()-1);			
+		}		
+		
+		//Down
+		if (playerX == this.getX() && playerY < this.getY()) {
+			moveBoulder(this.getX(), this.getY()+1);			
+		}
+	}
+	
 	public void moveBoulder(int x, int y) {
 		y().set(y);
 		x().set(x);
