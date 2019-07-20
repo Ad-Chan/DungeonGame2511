0b929b304400f8c09ea6bc987bb304001d00d4d1
Juhan <juhan.ambal@gmail.com>
2019-07-20 16:27:59 +1000

    Added timer for enemy

--- src/unsw/dungeon/Enemy.java
+++ src/unsw/dungeon/Enemy.java
@@ -1,10 +1,38 @@
 package unsw.dungeon;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 // Enemy Class (to be modified)
 public class Enemy extends Entity {
 	
 	public Enemy(int x, int y) {
 		super(x, y);
+		Timer timer = new Timer();
+		timer.schedule(new moveTimer(),0,5000);
 	}
 	
+    public void moveUp() {
+            y().set(getY() - 1);
+    }
+
+    public void moveDown() {
+            y().set(getY() + 1);
+    }
+
+    public void moveLeft() {
+            x().set(getX() - 1);
+    }
+
+    public void moveRight() {
+            x().set(getX() + 1);
+    }
+
+    class moveTimer extends TimerTask {
+
+    	@Override
+    	public void run() {
+    		Enemy.this.moveLeft();    		
+    	}
+    }
 }
