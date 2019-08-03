4ec43e52effb93d6265ca9b388260aa337467f24
Adrian <adrianccchan@gmail.com>
2019-08-03 16:56:45 +1000

    Added new method for exit

--- src/unsw/dungeon/Exit.java
+++ src/unsw/dungeon/Exit.java
@@ -5,4 +5,10 @@ public class Exit extends Entity {
     public Exit(int x, int y) {
         super(x, y);
     }   
+    
+    @Override
+    public boolean playerWalksInto(Player player) {
+    	//If Conditions are complete -> complete level
+    	return false;
+    }
 }
