bfeadac6c3e9e3bc88c94420982e5cb7bcccfcca
Juhan <juhan.ambal@gmail.com>
2019-08-03 23:10:06 +1000

    Added start screen code

--- src/unsw/dungeon/StartController.java
+++ src/unsw/dungeon/StartController.java
@@ -1,9 +1,9 @@
 package unsw.dungeon;
 
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -16,8 +16,9 @@ public class StartController {
 
     @FXML
     public void handleStartButton(ActionEvent event) throws IOException {
-    	Stage stage = new Stage();
-    	dungeonScreen.start(stage);
+    	Stage primaryStage = new Stage();
+    	Scene scene1;
+    	//startButton.setOnAction(value);
     }
     
     public void setCountScreen(DungeonApplication dungeonScreen) {
--- src/unsw/dungeon/StartScreen.java
+++ src/unsw/dungeon/StartScreen.java
@@ -1,36 +1,34 @@
 package unsw.dungeon;
 
 import java.io.IOException;
+
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class StartScreen {
+public class StartScreen extends Application{
 
-    private Stage stage;
-    private String title;
     private StartController controller;
-    private Scene scene;
 
-    public StartScreen(Stage stage) throws IOException {
-        this.stage = stage;
-        title = "Main Menu";
-
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Main Menu");
         controller = new StartController();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("start.fxml"));
         loader.setController(controller);
 
         // load into a Parent node called root
         Parent root = loader.load();
-        scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
-    public void start() {
-        stage.setTitle(title);
-        stage.setScene(scene);
-        stage.show();
-    }
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
     public StartController getController() {
         return controller;
